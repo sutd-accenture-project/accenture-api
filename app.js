869e4ff47b847c475263c98d87f4532901cf21d2
Samson Yu Bai Jian <samsonyubaijian@outlook.com>
2019-04-19 19:55:11 +0800

    Added dashboard name

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var cors = require('cors');
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  "credentials": true,
+  "origin": '*',
+  "methods": ['GET', 'PUT', 'POST', 'OPTIONS'],
+  "allowedHeaders": "Content-Type,Authorization"
+}));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
