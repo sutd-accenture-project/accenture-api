f27281240e1216a3126992377c95bc92259ec1c3
Samson Yu Bai Jian <samsonyubaijian@outlook.com>
2019-04-22 00:01:33 +0800

    Changed cors

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var cors = require('cors');
 
 var app = express();
 
-app.use(cors({
-	origin: 'http://localhost:3000'
-}));
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
